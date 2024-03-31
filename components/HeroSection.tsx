36dafb4b4d187304b864f97506924ca6c3b9837d
andjimenezuf <ajimenez1@ufl.edu>
2024-03-31 18:35:33 -0400

    lols

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -5,18 +5,44 @@ import { Link } from "react-scroll/modules"
 import { HiArrowDown } from "react-icons/hi"
 
 const HeroSection = () => {
+  const svgBackgroundPath = '/ccchaos.svg'
+
   return (
     <section id="home">
       <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
-        <div className="md:mt-2 md:w-1/2">
-          <Image
-            src="/headshot.jpg"
-            alt=""
-            width={300}
-            height={300}
-            className="rounded-full shadow-2xl"
-          />
-        </div>
+        
+            <div 
+            className="md:mt-2 md:w-1/2 relative" 
+            style={{ 
+              backgroundImage: `url(${svgBackgroundPath})`,
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: 'center',
+              backgroundSize: 'cover',
+              width: '400px', 
+              height: '400px', 
+            }}
+          >
+            {/* Position headshot with absolute values */}
+            <div 
+              className="absolute top-200 left-200" 
+              style={{
+                top: '100px', // 50 pixels from the top of the container
+                left: '100px', // 50 pixels from the left of the container
+              }}
+            >
+              <Image
+                src="/headshot.jpg"
+                alt="Headshot"
+                width={200}
+                height={200}
+                className="rounded-full shadow-2xl"
+              />
+            </div>
+          </div>
+
+            
+
+        
         <div className="md:mt-2 md:w-3/5">
           <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hello, I&#39;m Andres!</h1>
           <p className="text-lg mt-4 mb-6 md:text-2xl">
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -39,7 +39,7 @@ export default function Navbar() {
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
             <Link to="home">
               <div className="container flex items-center space-x-2">
-                <h2 className="text-2xl font-bold">Andres Jimenez</h2>
+                <h2 className="text-2xl font-bold  bg-gradient-to-tr from-blue-400 to-purple-600 bg-clip-text text-transparent">Andres Jimenez</h2>
               </div>
             </Link>
             <div className="md:hidden">
